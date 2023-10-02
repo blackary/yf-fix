1fd81b6f1bc88179ef2a4df1a61a846d950cf4a8
Zachary Blackwood <zachary@streamlit.io>
2023-10-02 12:16:58 -0400

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,14 @@ Path(CACHE_DIR).mkdir(exist_ok=True)
 import yfinance as yf
 import streamlit as st
 
+st.write("This has been fixed in the latest pre-release of yfinance.")
+st.code("pip install yfinance>=0.2.31b2")
+
 msft = yf.Ticker("MSFT")
 
 hist = msft.history(period="1mo")
 
 st.write(hist)
 
-st.markdown(Path("README.md").read_text())
+with st.expander("Old fix"):
+    st.markdown(Path("README.md").read_text())
