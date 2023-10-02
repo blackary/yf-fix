39b4215cf3789a5b50083e8ee02445c0a2b0395a
Zachary Blackwood <zachary@streamlit.io>
2023-10-02 12:15:17 -0400

    Use fixed version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+_ = """
 import appdirs as ad
 
 CACHE_DIR = ".cache"
@@ -9,6 +10,7 @@ ad.user_cache_dir = lambda *args: CACHE_DIR
 
 # Create the cache dir if it doesn't exist
 Path(CACHE_DIR).mkdir(exist_ok=True)
+"""
 
 import yfinance as yf
 import streamlit as st
